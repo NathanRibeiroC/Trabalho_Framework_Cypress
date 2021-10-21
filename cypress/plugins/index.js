234f8f8976d25b255cf4c29a2ee7b10f45149f8e
NathanRibeiroC <80288857+NathanRibeiroC@users.noreply.github.com>
2021-10-21 16:47:16 -0300

    Update index.js

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -21,12 +21,4 @@
    on('file:preprocessor', cucumber())
  }
  
- Cypress.on('uncaught:exception', (err, runnable) => {
 
-    // returning false here prevents Cypress from
-
-    // failing the test
-
-    return false
-
-  })
